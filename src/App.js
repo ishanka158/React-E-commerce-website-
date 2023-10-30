8762cc8cf831754d3f691e50930bb56564053ab2
ishanka158 <96714@fhss.sjp.ac.lk>
2023-10-30 08:45:40 +0530

    devolopment of main pages started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
     </div>
   );
 }
