c8d8ba34739104358a7a7b677757295f5e9a48b5
ishanka158 <96714@fhss.sjp.ac.lk>
2023-10-30 14:50:51 +0530

    fixed navbar issue and create hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Shop from './Pages/Shop.jsx';
+import ShopCategory from './Pages/ShopCategory.jsx';
+import Product from './Pages/Product.jsx';
+import Cart from './Pages/Cart.jsx';
+import LoginSighnup from './Pages/LoginSighnup';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Shop/>}></Route>
+            <Route path='/mens' element={<ShopCategory category='men'/>}></Route>
+            <Route path='/womens' element={<ShopCategory category='women'/>}></Route>
+            <Route path='/kids' element={<ShopCategory category='kid'/>}></Route>
+            <Route path='/product' element={<Product/>}>
+              <Route path=':productID' element={<Product/>}/>
+            </Route>
+            <Route path='/cart' element={<Cart/>}></Route>
+            <Route path='/login' element={<LoginSighnup/>}></Route>
+
+
+          </Routes>
+      </BrowserRouter>      
     </div>
   );
 }
---
+++ src/Pages/LoginSighnup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginSighnup() {
+  return (
+    <div>LoginSighnup</div>
+  )
+}
+
+export default LoginSighnup;
